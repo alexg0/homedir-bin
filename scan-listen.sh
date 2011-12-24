b2b777de3a912d241a80b4d7eeb35ff74968bb45
alexg0 <alexg@alexland.org>
2011-12-24 15:59:12 -0600

    scan script provides status

--- scan-listen.sh
+++ scan-listen.sh
@@ -5,7 +5,12 @@ scanner=prn-3rd-fl
 me=$(hostname -s|sed s/-//)
 
 mkdir -p $d || exit 1
-screen -S dell1600 -d -m -L \
-  dell1600n-net-scan.pl \
-  --1815dn --multi-session --listen $scanner --name $me \
-  --scan-dir $d
+screen -ls | grep dell1600 && \
+    echo "Already running, not starting." || \
+    screen -S dell1600 -d -m -L \
+         dell1600n-net-scan.pl \
+  	  --1815dn --multi-session --listen $scanner --name $me \
+          --scan-dir $d
+
+echo "Scan daemon status:"
+screen -ls | grep dell1600
