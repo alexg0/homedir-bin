cf3b067307bec7be5b6cab652161bada0effd1b2
Alexander Goldstein <alexg@alexland.org>
2015-10-01 17:30:10 -0500

    forward-ports

---
+++ forward-ports-eligo.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+ssh_server=alexg@serv01.eligoenergy.com
+ssh_server=serv01.eligoenergy.com
+
+autossh_args="-f"
+ssh_args="-C -N"
+
+local=("-L 54323:whdb.eligoenergy.com:5432"
+       "-L 54324:whdb.eligoenergy.com:5432"
+       )
+
+all_ports="${local[*]}"
+
+AUTOSSH_PORT=27027 autossh ${autossh_args} ${all_ports} ${ssh_server} ${ssh_args}
+
